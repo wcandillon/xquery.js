00966f398e8af9037cd9411b7f9f59b4e4377e22
William Candillon <wcandillon@gmail.com>
2012-05-31 18:44:06 +0200

    Update test suite.

--- run_tests.js
+++ run_tests.js
@@ -1,48 +1,78 @@
 var walk = require('walk');
 var fs = require('fs');
 var requirejs = require('requirejs');
+var xquery = requirejs('./build/xquery');
 
 String.prototype.endsWith = function(suffix) {
   return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
 
-function main(args) {
-  // Walker options
-  var walker  = walk.walk('./queries', { followLinks: false });
-  var xquery = requirejs('./build/xquery');
+var successes = [];
+var failures  = [];
 
-  walker.on('file', function(root, stat, next) {
-    // Add this file to the list of files
-    var filename = root + '/' + stat.name;
-    if(filename.endsWith(".xq")) {
-      console.log(filename);
-      var code = fs.readFileSync(filename, "UTF-8");
-      //output += "<pre><![CDATA[" + code + "]]></pre>";
-      var parser = xquery.getParser(code);
-      var ast = parser.p_Module();
-      parser.highlighter.getTokens();
-      //console.log(parser.highlighter.getTokens());
-     if(parser.hasErrors()) {
-        var errors = parser.getErrors();
-        for(i in errors) {
-          var error = errors[i];
-          console.log(error);
-          var problem = {
-            row: error.line,
-            column: error.column,
-            text: error.message,
-            type: "error"
-          };
-          console.log(problem);
-        }
-      }   
+function parseFile(filename, failOnError)
+{
+  console.log("Parse: " + filename);
+  var code = fs.readFileSync(filename, "UTF-8");
+  var parser = xquery.getParser(code);
+  var ast = parser.p_Module();
+  parser.highlighter.getTokens();
+ if(parser.hasErrors() && !failOnError) {
+    var errors = parser.getErrors();
+    for(i in errors) {
+      var error = errors[i];
+      console.log(error);
+      var problem = {
+        row: error.line,
+        column: error.column,
+        text: error.message,
+        type: "error"
+      };
+      console.log(problem);
     }
-    next();
-  });
+    return;
+  } else if(parser.hasErrors()) {
+    failures.push(filename);
+  } else {
+    successes.push(filename);
+  }
+}
 
-  walker.on('end', function() {
-    //console.log(files);
-  });
+function main(args) {
+  var keepGoing =  args.indexOf("--keep-going") != -1;
+  var file      =  args.indexOf("-f");
+  var path      = "./queries";
+  if(file != -1)
+  {
+    if(args.length <= (file + 1)) {
+      throw "Missing argument to -f: -f <filename>"; 
+    }
+    var filename = args[file + 1];
+    path = "./" + filename;
+    parseFile(filename, keepGoing);
+  } else {
+    var walker  = walk.walk(path, { followLinks: false });
+    
+    walker.on('file', function(root, stat, next) {
+      // Add this file to the list of files
+      var filename = root + '/' + stat.name;
+      if(filename.endsWith(".xq")) {
+        parseFile(filename, keepGoing);
+      }
+      next();
+    });
+
+    walker.on('end', function() {
+      console.log("Parsed " + (failures.length + successes.length) + " files.");
+      console.log(successes.length + " succeeded, " + failures.length + " failed.");
+      console.log("The following files didn't parse: ");
+      var i;
+      for(i in failures)
+      {
+        console.log(failures[i]);
+      }
+    });
+  }
 };
 
 main(process.argv);
